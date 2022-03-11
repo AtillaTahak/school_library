7e0f78dfdac531fcd58bcb6ba55473e251413243
atillatahak <atillatahaa@gmail.com>
2022-03-11 16:41:19 +0300

    fixed

--- student.rb
+++ student.rb
@@ -1,6 +1,6 @@
 class Student < Person
-  def initialize(classroom)
-    super
+  def initialize(classroom, age, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < Person
-  def initialize(specialization)
-    super
+  def initialize(specialization, age, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
